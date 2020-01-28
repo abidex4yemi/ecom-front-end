48c4a193d3c8976fcd1a9e9fb49d35834e96fe12
abidex4yemi <abidex4yemi@gmail.com>
2020-01-28 12:40:46 +0100

    Add: host backend url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'http://localhost:2020/api/v1';
+export const BASE_API_URL = 'https://backend-venco-ecom.herokuapp.com/api/v1';
 export const CLOUDINARY_URL =
   'https://api.cloudinary.com/v1_1/din5avzpb/image/upload';
 // Note: this are being expose for test purposes
